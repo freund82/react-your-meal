d5cb5102ace8c3cb663e6c2c38f842ba6b02deb3
Maxim Utkin <maxim2001_2001@mail.ru>
2024-10-26 22:33:15 +0300

    withHeader

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-
+import Header from "./components/Header/Header"
 
 
 function App() {
   
   return (
     <>
-      <h1>Тест</h1>
+      <Header />
     </>
   )
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,24 @@
+import "./header.css"
+import Logo from "../../assets/icons/logoWithIcons.png"
+import BurgerImg from "../../assets/icons/burger.png"
+
+function Header() {
+    return (
+        <header className="header">
+            <div className="header__logo">
+                <img src={Logo} alt="logo" />
+            </div>
+            <div className="header__inner">
+                    <img className="header__burgerImg" src={BurgerImg} alt="burger"/>
+                <div className="header__title">
+                    <h1 style={{ color:"var(--white-color)"}}>Только самые <span style={{color: "var(--orange-color)"}}>сочные бургеры!</span></h1>
+                    <div className="header__subtitle">
+                        <p style={{ color:"var(--white-color)", marginTop:"6rem"}}>Бесплатная доставка от 599₽</p>
+                     </div>
+                </div>
+                </div>
+        </header>
+    )
+}
+
+export default Header
